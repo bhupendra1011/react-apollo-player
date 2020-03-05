bf3fd6b467a787b7912c3cbdb9b218f75e352131
Bhupendra <bhupendra1011@gmail.com>
2020-03-05 23:16:47 +0530

    Added Song compo & Dialog to edit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import SongPlayer from "./components/SongPlayer";
 import { Grid } from "@material-ui/core";
 
 function App() {
-	return (
-		<>
-			<Header />
-			<Grid container spacing={3}>
-				<Grid item xs={12} md={7}>
-					<AddSong />
-					<SongList />
-				</Grid>
-				<Grid item xs={12} md={5}>
-					<SongPlayer />
-				</Grid>
-			</Grid>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Grid style={{ marginTop: 80 }} container spacing={3}>
+        <Grid item xs={12} md={7}>
+          <AddSong />
+          <SongList />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <SongPlayer />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,6 +1,98 @@
 import React from "react";
+import {
+  TextField,
+  InputAdornment,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  makeStyles
+} from "@material-ui/core";
+import { Link, AddBoxOutlined } from "@material-ui/icons";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    alignItems: "center"
+  },
+  urlInput: {
+    margin: theme.spacing(1)
+  },
+  addSongButton: {
+    margin: theme.spacing(1)
+  },
+
+  dialog: {
+    textAlign: "center"
+  },
+  thumbnail: {
+    width: "90%"
+  }
+}));
 
 function AddSong() {
-	return <div>AddSong</div>;
+  const [dialog, setOpen] = React.useState(false);
+  const handleDialogClose = () => setOpen(false);
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <Dialog
+        className={classes.dialog}
+        open={dialog}
+        onClose={handleDialogClose}
+      >
+        <DialogTitle>Edit Song</DialogTitle>
+        <DialogContent>
+          <img
+            src="https://cdn-images-1.medium.com/max/1200/1*yDnqkp49pwGu7Ezy1sRc5w.png"
+            alt="sample "
+            className={classes.thumbnail}
+          />
+
+          <TextField margin="dense" name="title" label="Title" fullWidth />
+          <TextField margin="dense" name="artist" label="Artist" fullWidth />
+          <TextField
+            margin="dense"
+            name="thumbnail"
+            label="Thumbnail"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button color="secondary" onClick={handleDialogClose}>
+            Cancel
+          </Button>
+          <Button variant="outlined" color="primary">
+            Add Song
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <TextField
+        margin="normal"
+        type="url"
+        fullWidth
+        className={classes.urlInput}
+        placeholder="Add youtube or soundcloud url"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Link />
+            </InputAdornment>
+          )
+        }}
+      ></TextField>
+      <Button
+        className={classes.addSongButton}
+        onClick={() => setOpen(true)}
+        variant="contained"
+        color="primary"
+        endIcon={<AddBoxOutlined />}
+      >
+        {" "}
+        Add
+      </Button>
+    </div>
+  );
 }
 export default AddSong;
