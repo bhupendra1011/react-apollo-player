98483c80c4f313042d5578b97272ad946305bdb6
Bhupendra <bhupendra1011@gmail.com>
2020-05-15 21:59:37 +0530

    updatd theme color

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,22 @@ import React from "react";
 import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
 import { HeadsetMicTwoTone } from "@material-ui/icons";
 
-const useStyles = makeStyles(theme => ({
-	title: {
-		marginLeft: theme.spacing(2)
-	}
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginLeft: theme.spacing(2),
+  },
 }));
 function Header() {
-	const classes = useStyles();
-	return (
-		<AppBar color='primary' position='fixed'>
-			<Toolbar>
-				<HeadsetMicTwoTone />
-				<Typography className={classes.title} variant='h6' component='h1'>
-					Apollo Music Player
-				</Typography>
-			</Toolbar>
-		</AppBar>
-	);
+  const classes = useStyles();
+  return (
+    <AppBar color="primary" position="fixed">
+      <Toolbar>
+        <HeadsetMicTwoTone />
+        <Typography className={classes.title} variant="h6" component="h1">
+          My Music Share
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 export default Header;
